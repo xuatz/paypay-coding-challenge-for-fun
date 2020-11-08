1986be8a942588c068c5935227e5fb17420e8564
xuatz <xzlow10@gmail.com>
2020-11-09 01:19:44 +0900

    chore: housekeeping

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,31 +1,40 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
+import Employees from './components/Employees/Employees';
+import PerformanceReviews from './components/PerformanceReviews/PerformanceReviews';
 import './App.css';
 
 function App() {
-  const isLoggedIn = false;
+  const isNotLoggedIn = true;
 
-  return isLoggedIn ? (
-    <LoginForm />
-  ) : (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/admin/employees">
-            <h1>List of Employees</h1>
-          </Route>
-          <Route path="/admin/reviews">
-            <h1>List of Performance Reviews</h1>
-          </Route>
-          <Route path="/reviews">
-            <h1>List of Performance Reviews assigned for feedback</h1>
-          </Route>
-          <Redirect to="/reviews" />
-        </Switch>
-      </div>
-    </Router>
-  );
+  return (
+    <>
+      <h1>Super Feedback Submitter</h1>
+      {!isNotLoggedIn ? (
+        <LoginForm />
+      ) : (
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/admin/employees">
+                <Employees />
+              </Route>
+              <Route path="/admin/reviews">
+                <PerformanceReviews />
+              </Route>
+              <Route path="/reviews">
+                <h2>List of Performance Reviews assigned for feedback</h2>
+                <p>Not implemented</p>
+              </Route>
+              <Redirect to="/reviews" />
+            </Switch>
+          </div>
+        </Router>
+      )}
+    </>
+
+  )
 }
 
 export default App;
